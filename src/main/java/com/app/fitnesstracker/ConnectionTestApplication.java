b82c57e901f97dba09db5257a17ab0538c7a86e4
Pavneet20 <109867426+Pavneet20@users.noreply.github.com>
2025-05-08 22:21:47 -0400

    login page setup

--- src/main/java/com/app/fitnesstracker/ConnectionTestApplication.java
+++ src/main/java/com/app/fitnesstracker/ConnectionTestApplication.java
@@ -1,3 +1,5 @@
+package com.app.fitnesstracker;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
--- src/test/java/com/app/fitnesstracker/FitnessTrackerApplicationTests.java
+++ src/test/java/com/app/fitnesstracker/FitnessTrackerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FitnessTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
